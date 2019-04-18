41fbdf223260431573113b902b04a1da81969844
shahMoiz <abdul_moiz10@hotmail.com>
2019-04-18 00:21:31 -0700

    Topbar styling

--- src/screens/findPlaces/findPlaces.js
+++ src/screens/findPlaces/findPlaces.js
@@ -1,11 +1,65 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
+import { Navigation } from "react-native-navigation";
+import AntDesignIcon from 'react-native-vector-icons/AntDesign';
 
 class FindPlacesScreen extends Component {
+    constructor(props){
+        super(props);
+        // Navigation.events().bindComponent(this);
+    }
+    // a =  AntDesignIcon.getImageSource("bars", 30, 'black')
+    static get options() {
+        // Promise.all([
+        //     AntDesignIcon.getImageSource("bars", 30, 'black')
+        // ]).then (source => {
+            return {
+            topBar: {
+                title: {
+                    text: 'View Awesome Places'
+                },
+                subtitle: {
+                    text: 'Check out Best Places in your Town',
+                    color: 'gray'
+                },
+                // leftButtons: [{
+                //     id: 'sideDraw',
+                //     component: {
+                //         name: 'Awesome-places.AuthScreen',
+                //     },
+                //     passProps: {
+                //         myFunctions: () => Navigation.mergeOptions('sideDraw', {
+                //             sideMenu: {
+                //                 left: {
+                //                 visible: true
+                //                 }
+                //             }
+                //             })
+                //     }
+                    
+                // }]
+            }
+        }
+        // });
+      }
+
+    myFunction = () => {
+        Navigation.mergeOptions('sideDraw', {
+            sideMenu: {
+              left: {
+                visible: true
+              }
+            }
+          });
+    }
     render(){
         return (
             <View>
                 <Text>Find Places Run</Text>
+                <Button 
+                    title="Check"
+                    onPress={this.myFunction}
+                />
             </View>
         )
     }
--- src/screens/indexTabs/main.js
+++ src/screens/indexTabs/main.js
@@ -2,63 +2,182 @@ import React, {Component} from 'react';
 // import { Promise } from 'react-native';
 import { Navigation } from "react-native-navigation";
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import AntDesignIcon from 'react-native-vector-icons/AntDesign';
 
 const MainTabs = () =>{
     Promise.all([
     Icon.getImageSource("map-marker", 30, 'red'),
-    Icon.getImageSource("view-dashboard", 30, 'blue')
+    Icon.getImageSource("view-dashboard", 30, 'blue'),
+    AntDesignIcon.getImageSource("bars", 30, 'black')
     ]).then(img => {
         Navigation.setRoot({
             root: {
-                bottomTabs: {
-                    children: [{
-                        stack: {
-                            children: [{
-                                component: {
-                                    name: 'Awesome-places.findPlaces',
-                                }
-                            }],
-                            options: {
-                                bottomTab: {
-                                    text: 15,
-                                    text: "Find Place",
-                                    icon: img[0]
-                                },
-                                topBar: {
-                                    title: {
-                                        text: 'View Awesome Places'
-                                    }
-                                }
-                            }
-                        }
+              sideMenu: {
+                left: {
+                    visible: true,
+                  component: {
+                    name: 'Awesome-places.viewPlaces',
+                    passProps: {
+                      text: 'This is a left side menu screen'
                     },
-                    {
-                        stack: {
-                            children: [{
-                                component: {
-                                    name: 'Awesome-places.viewPlaces',
-                                    options: {
+                    id: 'sideDraw',
+                    
+                  }
+                },
+                center: {
+                    bottomTabs: {
+                                    children: [{
                                         
-                                    }
+                                        stack: {
+                                            
+                                            children: [{
+                                                
+                                                component: {
+                                                    name: 'Awesome-places.findPlaces',
+                                                }
+                                            }],
+                                            options: {
+                                                bottomTab: {
+                                                    text: "Find Place",
+                                                    icon: img[0]
+                                                },
+                                                topBar: {
+                                                //     title: {
+                                                //         text: 'View Awesome Places'
+                                                //     },
+                                                //     subtitle: {
+                                                //         text: 'Check out Best Places in your Town',
+                                                //         color: 'gray'
+                                                //     },
+                                                    // leftButtons: [{
+                                                    //     icon: source[0],
+                                                //         id: 'sideDraw',
+                                                //         icon: img[2],
+                                                //         component: {
+                                                //             name: 'Awesome-places.AuthScreen',
+                                                //         },
+                                                //         passProps: {
+                                                //             myFunctions: () => Navigation.mergeOptions('sideDraw', {
+                                                //                 sideMenu: {
+                                                //                   left: {
+                                                //                     visible: true
+                                                //                   }
+                                                //                 }
+                                                //               })
+                                                //         }
+                                                        
+                                                    // }]
+                                                }
+                                            }
+                                        }
+                                    },
+                                    {
+                                        stack: {
+                                            children: [{
+                                                component: {
+                                                    name: 'Awesome-places.viewPlaces',
+                                                    options: {
+                                                        
+                                                    }
+                                                }
+                                            }],
+                                            options: {
+                                                bottomTab: {
+                                                    text: 15,
+                                                    text: 'Places',
+                                                    icon: img[1]
+                                                },
+                                                topBar: {
+                                                    title: {
+                                                        text: 'Share your Awesome Visited Places'
+                                                    }
+                                                }
+                                            }
+                                        }  
+                                    }]   
                                 }
-                            }],
-                            options: {
-                                bottomTab: {
-                                    text: 15,
-                                    text: 'Places',
-                                    icon: img[1]
-                                },
-                                topBar: {
-                                    title: {
-                                        text: 'Share your Awesome Visited Places'
-                                    }
-                                }
-                            }
-                        }  
-                    }]   
+                },
+                right: {
+                  component: {
+                    name: 'Awesome-places.AuthScreen',
+                    passProps: {
+                      text: 'This is a right side menu screen'
+                    }
+                  }
                 }
+              }
             }
-        })
+          });
+        // Navigation.setRoot({
+        //     root: {
+                
+        //         bottomTabs: {
+        //             children: [{
+                        
+        //                 stack: {
+                            
+        //                     children: [{
+                                
+        //                         component: {
+        //                             name: 'Awesome-places.findPlaces',
+        //                         }
+        //                     }],
+        //                     options: {
+        //                         bottomTab: {
+        //                             text: "Find Place",
+        //                             icon: img[0]
+        //                         },
+        //                         topBar: {
+        //                             title: {
+        //                                 text: 'View Awesome Places'
+        //                             },
+        //                             subtitle: {
+        //                                 text: 'Check out Best Places in your Town',
+        //                                 color: 'gray'
+        //                             },
+        //                             leftButtons: [{
+        //                                 id: 'side Draw',
+        //                                 icon: img[2],
+        //                                 component: {
+        //                                     name: 'Awesome-places.AuthScreen',
+        //                                     passProps: {
+                                                
+        //                                     }
+                                            
+        //                                 },
+                                        
+        //                             }]
+        //                         }
+        //                     }
+        //                 }
+        //             },
+        //             {
+        //                 stack: {
+        //                     children: [{
+        //                         component: {
+        //                             name: 'Awesome-places.viewPlaces',
+        //                             options: {
+                                        
+        //                             }
+        //                         }
+        //                     }],
+        //                     options: {
+        //                         bottomTab: {
+        //                             text: 15,
+        //                             text: 'Places',
+        //                             icon: img[1]
+        //                         },
+        //                         topBar: {
+        //                             title: {
+        //                                 text: 'Share your Awesome Visited Places'
+        //                             }
+        //                         }
+        //                     }
+        //                 }  
+        //             }]   
+        //         }
+        //     }
+        // })
     })
     
 }
